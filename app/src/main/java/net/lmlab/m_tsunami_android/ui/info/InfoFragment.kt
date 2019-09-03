848b0108a2730f567fe4ad9f143c88ad7d3e612e
azaz-el <sear.azazel@gmail.com>
2019-09-04 01:10:20 +0900

    feat: バージョン番号を表示する

--- app/src/main/java/net/lmlab/m_tsunami_android/ui/info/InfoFragment.kt
+++ app/src/main/java/net/lmlab/m_tsunami_android/ui/info/InfoFragment.kt
@@ -23,15 +23,18 @@ class InfoFragment : Fragment() {
     ): View? {
         infoViewModel =
             ViewModelProviders.of(this).get(InfoViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
-        infoViewModel.text.observe(this, Observer {
-
-            textView.text = it
+        val root = inflater.inflate(R.layout.fragment_info, container, false)
+        val linkTextView: TextView = root.findViewById(R.id.text_info)
+        infoViewModel.linkText.observe(this, Observer {
+            linkTextView.text = it
             val githubUrl = "https://github.com/codeformiyazaki/m-tsunami-android"
             val pattern = Pattern.compile("GitHub")
             val filter = Linkify.TransformFilter { _, _ -> githubUrl }
-            Linkify.addLinks(textView, pattern, githubUrl, null, filter)
+            Linkify.addLinks(linkTextView, pattern, githubUrl, null, filter)
+        })
+        val versionTextView: TextView = root.findViewById(R.id.text_version)
+        infoViewModel.versionText.observe(this, Observer {
+            versionTextView.text = it
         })
         return root
     }
--- app/src/main/java/net/lmlab/m_tsunami_android/ui/info/InfoViewModel.kt
+++ app/src/main/java/net/lmlab/m_tsunami_android/ui/info/InfoViewModel.kt
@@ -3,10 +3,16 @@ package net.lmlab.m_tsunami_android.ui.info
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import net.lmlab.m_tsunami_android.BuildConfig
 
 class InfoViewModel:ViewModel() {
-    private val _text = MutableLiveData<String>().apply {
+    private val _linkText = MutableLiveData<String>().apply {
         value = "Find us on GitHub"
     }
-    val text: LiveData<String> = _text
+    val linkText: LiveData<String> = _linkText
+
+    private val _versionText = MutableLiveData<String>().apply {
+        value = "version " + BuildConfig.VERSION_NAME
+    }
+    val versionText: LiveData<String> = _versionText
 }
