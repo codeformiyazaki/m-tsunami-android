84c9549de0f51be1fba3d7de7b7a864ba8e7862e
koogawa <koogawa.app@gmail.com>
2019-10-01 00:35:38 +0900

    GISタブを実装

--- app/src/main/java/net/lmlab/m_tsunami_android/ui/hinata/HinataFragment.kt
+++ app/src/main/java/net/lmlab/m_tsunami_android/ui/hinata/HinataFragment.kt
@@ -5,7 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import net.lmlab.m_tsunami_android.R
+import android.webkit.WebView
 
 class HinataFragment : Fragment() {
 
@@ -14,7 +14,13 @@ class HinataFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val root = inflater.inflate(R.layout.fragment_hinata, container, false)
-        return root
+        val rootView = inflater.inflate(net.lmlab.m_tsunami_android.R.layout.fragment_hinata, container, false)
+
+        val url = "https://kenzkenz.xsrv.jp/aaa/#13/131.44455/31.92127%3FS%3D1%26L%3D%5B%5B%7B%22id%22%3A%22shinsuishin%22%2C%22o%22%3A1%7D%2C%7B%22id%22%3A1%2C%22o%22%3A1%7D%5D%2C%5B%7B%22id%22%3A2%2C%22o%22%3A1%2C%22c%22%3A%22%22%7D%5D%2C%5B%7B%22id%22%3A4%2C%22o%22%3A1%2C%22c%22%3A%22%22%7D%5D%2C%5B%7B%22id%22%3A5%2C%22o%22%3A1%2C%22c%22%3A%22%22%7D%5D%5D"
+        val view = rootView.findViewById(net.lmlab.m_tsunami_android.R.id.webView) as WebView
+        view.settings.javaScriptEnabled = true
+        view.loadUrl(url)
+
+        return rootView
     }
 }
