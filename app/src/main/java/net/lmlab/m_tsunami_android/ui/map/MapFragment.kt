de73e6032bb14c8f9e1c6b9cd06da238f90f503c
koogawa <koogawa.app@gmail.com>
2019-10-16 01:13:22 +0900

    地図を表示して色々置いてみる

--- app/src/main/java/net/lmlab/m_tsunami_android/ui/map/MapFragment.kt
+++ app/src/main/java/net/lmlab/m_tsunami_android/ui/map/MapFragment.kt
@@ -1,14 +1,22 @@
 package net.lmlab.m_tsunami_android.ui.map
 
+import android.Manifest
+import android.annotation.TargetApi
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.PermissionChecker.checkPermission
 import androidx.fragment.app.Fragment
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.MapView
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.MapsInitializer
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.LatLng
+import androidx.core.app.ActivityCompat.requestPermissions
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.MarkerOptions
+
 
 class MapFragment : Fragment(), OnMapReadyCallback {
 
@@ -18,6 +26,7 @@ class MapFragment : Fragment(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap?) {
         if (googleMap != null) {
             this.googleMap = googleMap
+            doCheckPermission()
         }
     }
 
@@ -32,6 +41,7 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         mapView = rootView.findViewById(net.lmlab.m_tsunami_android.R.id.mapView)
         mapView.onCreate(savedInstanceState)
         mapView.getMapAsync(this@MapFragment)
+
 
         return rootView
     }
@@ -69,5 +79,34 @@ class MapFragment : Fragment(), OnMapReadyCallback {
     override fun onLowMemory() {
         super.onLowMemory()
         mapView.onLowMemory()
+    }
+
+    private fun doCheckPermission() {
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(requireActivity(), arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 10) //REQUEST_LOCATION
+        } else {
+            Log.e("DB", "PERMISSION GRANTED")
+            // この５行を実行するとクラッシュ
+//            googleMap.isMyLocationEnabled = true
+//            googleMap.uiSettings.isMapToolbarEnabled = true
+//            val osakaStation = googleMap.getMyLocation()
+//            val myLatLng = LatLng(osakaStation.getLatitude(), osakaStation.getLongitude())
+//            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLatLng, 16.0f))
+
+            val sydney = LatLng(-34.0, 151.0)
+            googleMap.addMarker(
+                MarkerOptions()
+                    .position(sydney)             // 地図上のマーカーの位置
+                    .title("Marker in Sydney")    // マーカーをタップ時に表示するテキスト文字列
+                    .snippet("Australian cities") // タイトルの下に表示される追加のテキスト
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)) // アイコン
+            )
+        }
+    }
+
+    //許可を求めるダイアログで何らかのボタンが押されたときに発生するイベント
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        Log.e("DB", "onRequestPermissionsResult")
     }
 }
