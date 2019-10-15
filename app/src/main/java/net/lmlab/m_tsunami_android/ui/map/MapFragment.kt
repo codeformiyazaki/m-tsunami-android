bbae84edd68a1a1690e001207cba6a2a630db6a9
koogawa <koogawa.app@gmail.com>
2019-10-15 21:42:57 +0900

    マップ画面に必要なライフサイクルを実装

--- app/src/main/java/net/lmlab/m_tsunami_android/ui/map/MapFragment.kt
+++ app/src/main/java/net/lmlab/m_tsunami_android/ui/map/MapFragment.kt
@@ -5,8 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.MapsInitializer
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback {
+
+    private lateinit var mapView: MapView
+    private lateinit var googleMap: GoogleMap
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        if (googleMap != null) {
+            this.googleMap = googleMap
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,8 +28,46 @@ class MapFragment : Fragment() {
     ): View? {
         val rootView = inflater.inflate(net.lmlab.m_tsunami_android.R.layout.fragment_map, container, false)
 
-        // todo
+        MapsInitializer.initialize(this.activity)
+        mapView = rootView.findViewById(net.lmlab.m_tsunami_android.R.id.mapView)
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this@MapFragment)
 
         return rootView
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
     }
 }
