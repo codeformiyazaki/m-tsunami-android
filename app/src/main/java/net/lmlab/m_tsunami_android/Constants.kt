28c35bcfad8e492863e71fe50a894ff758c0e40d
koogawa <koogawa.app@gmail.com>
2019-10-20 23:15:19 +0900

    Fix #10 gis のロードに時間がかかる

--- app/src/main/java/net/lmlab/m_tsunami_android/Constants.kt
+++ app/src/main/java/net/lmlab/m_tsunami_android/Constants.kt
@@ -2,4 +2,5 @@ package net.lmlab.m_tsunami_android
 
 object Constants {
     const val GOOGLE_API_KEY = "YOUR API KEY"
+    const val HINATA_URL = "https://kenzkenz.xsrv.jp/aaa/#13/131.44455/31.92127%3FS%3D1%26L%3D%5B%5B%7B%22id%22%3A%22shinsuishin%22%2C%22o%22%3A1%7D%2C%7B%22id%22%3A1%2C%22o%22%3A1%7D%5D%2C%5B%7B%22id%22%3A2%2C%22o%22%3A1%2C%22c%22%3A%22%22%7D%5D%2C%5B%7B%22id%22%3A4%2C%22o%22%3A1%2C%22c%22%3A%22%22%7D%5D%2C%5B%7B%22id%22%3A5%2C%22o%22%3A1%2C%22c%22%3A%22%22%7D%5D%5D"
 }
